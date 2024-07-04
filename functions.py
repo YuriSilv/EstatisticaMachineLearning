6c279ce46da5d2d822fe5ca9407aec0a19f646d7
YuriSilv <yuridtna2002@hotmail.com>
2024-07-04 14:46:23 -0300

    dispersão

--- functions.py
+++ functions.py
@@ -1,3 +1,5 @@
+import math
+
 def mean(values:list):
     num = sum(values)
     den = len(values)
@@ -31,3 +33,43 @@ def mode(values:list):
     most_freqs = [v[0] for v in most_commons if v[1] == biggest_value]
 
     return set(most_freqs)
+
+
+def quartil(values:list, percentil):
+    num_values = len(values)
+    center_pos = int((num_values + 1) / 2) 
+
+    if percentil == 0.5:
+        return median(values)
+    elif percentil == 0.25:
+        values_quantil = values[:center_pos]
+        return median(values_quantil)
+    elif percentil == 0.75:
+        values_quantil = values[center_pos:]
+        return median(values_quantil)
+    else:
+        print('Insira um quartil válido - 0.25, 0.5, 0.75')
+
+def amplitude(values:list):
+    max_v = max(values)
+    min_v = min(values)
+    range_v = max_v - min_v
+    return range_v
+
+def iqr(values:list):
+    result = quartil(values, 0.75) - quartil(values, 0.25)
+    return result
+
+def variance(values:list, is_population = False):
+    mean_value = mean(values)
+
+    num = sum([(value - mean_value)**2 for value in values])
+    den = len(values) - 1
+
+    if is_population:
+        den = den + 1
+        
+    return (num/den)
+
+def std(values:list, is_population = False ):
+    return math.sqrt(variance(values, is_population))
